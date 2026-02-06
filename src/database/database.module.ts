5f9e6a0cd35ab7b5f1d79749bc41c9998c653090
desta-elias <joy730671@gmail.com>
2026-02-06 14:21:14 +0300

    update backend

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -32,6 +32,8 @@ import { seedBeds } from './seed-beds';
           database,
           synchronize: false, // only for dev
           entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+          migrationsRun: true,
           ssl: false, // important for local Docker
         };
       },
