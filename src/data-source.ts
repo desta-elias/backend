334b22186182e61aae7bdecb1b327a0280ab4503
desta-elias <joy730671@gmail.com>
2026-02-09 15:18:29 +0300

    update backend

---
+++ src/data-source.ts
@@ -0,0 +1,21 @@
+require('reflect-metadata');
+const { DataSource } = require('typeorm');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const AppDataSource = new DataSource({
+  type: process.env.DB_TYPE || 'postgres',
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: true,
+  entities: [path.join(__dirname, 'dist/**/*.entity{.ts,.js}')],
+  migrations: [path.join(__dirname, 'dist/migrations/*{.ts,.js}')],
+});
+
+module.exports = { AppDataSource };
